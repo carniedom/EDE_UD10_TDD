7473eec4df0709bcb173c488627ed77d75c10993
carniedom <carniedom@alu.edu.gva.es>
2023-04-02 12:33:51 +0200

    Refactorizado nombre Métodos
    Añado mi nombre a los métodos

--- TDD Practica Coche/src/Coche.java
+++ TDD Practica Coche/src/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void Acelerar(int aceleracion) {
+    public void Acelerar_CarmeloNietoDomene(int aceleracion) {
         velocidad += aceleracion;
     }
 
-    public void Decelerar(int deceleracion){
+    public void Decelerar_CarmeloNietoDomene(int deceleracion){
         velocidad -= deceleracion;
         if (velocidad < 0){
             velocidad = 0;
--- TDD Practica Coche/tests/TestCoche.java
+++ TDD Practica Coche/tests/TestCoche.java
@@ -3,28 +3,28 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void Test_Al_Crear_Un_Coche_Su_Velocidad_Es_Cero(){
+    public void Test_Al_Crear_Un_Coche_Su_Velocidad_Es_Cero_CarmeloNietoDomene(){
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
     @Test
-    public void Test_Al_Acelerar_Un_Coche_Su_Velocidad_Aumenta(){
+    public void Test_Al_Acelerar_Un_Coche_Su_Velocidad_Aumenta_CarmeloNietoDomene(){
         Coche nuevoCoche = new Coche();
-        nuevoCoche.Acelerar(30);
+        nuevoCoche.Acelerar_CarmeloNietoDomene(30);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
     @Test
-    public void Test_Al_Decelerar_Un_Coche_Su_Velocidad_Disminuye(){
+    public void Test_Al_Decelerar_Un_Coche_Su_Velocidad_Disminuye_CarmeloNietoDomene(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.Decelerar(20);
+        nuevoCoche.Decelerar_CarmeloNietoDomene(20);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
     @Test
-    public void Test_Al_Decelerar_Un_Coche_Su_Velocidad_No_Puede_Ser_Menor_Que_Cero(){
+    public void Test_Al_Decelerar_Un_Coche_Su_Velocidad_No_Puede_Ser_Menor_Que_Cero_CarmeloNietoDomene(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.Decelerar(80);
+        nuevoCoche.Decelerar_CarmeloNietoDomene(80);
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 }
