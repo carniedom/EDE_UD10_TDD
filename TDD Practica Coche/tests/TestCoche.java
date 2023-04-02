46b0a560bb16c9a747578de97796c46528d06eb4
carniedom <carniedom@alu.edu.gva.es>
2023-04-02 11:42:28 +0200

    TestCoche modificado
    Se modifica Test_Crear_Coche con su versión mejorada

--- TDD Practica Coche/tests/TestCoche.java
+++ TDD Practica Coche/tests/TestCoche.java
@@ -1,8 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void Test_Crear_Coche(){
+    public void Test_Al_Crear_Un_Coche_Su_Velocidad_Es_Cero(){
         Coche nuevoCoche = new Coche();
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 }
